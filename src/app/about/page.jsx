b9a02bf1e81752f1879999f25b694a4233330d93
João Paulo Moura Clevelares <joaopaulomoura5328@gmail.com>
2024-01-23 12:35:48 -0300

    :construction: Building about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,78 @@
+"use client"
+
 import Image from "next/image";
 import styles from "./about.module.css"
+import { useEffect, useState } from "react";
 
 const AboutPage = () => {
+
+  const [expNumber, setExpNumber] = useState(0)
+  const [reached, setReached] = useState(0)
+  const [plugins, setPlugins] = useState(0)
+  const [isBright, setBright] = useState(false)
+
+  useEffect(() => {
+
+    const interval = setInterval(() => {
+      setExpNumber((prev) => {
+        if(prev < 20){
+          prev += 1
+        } else{
+          prev += 0
+          clearInterval(interval)
+        }
+        return prev
+      })
+      const interval2 = setInterval(() => {
+        setReached((prev) => {
+            if(prev < 1000){
+              prev += 1
+            } else{
+              prev += 0
+              clearInterval(interval2)
+              setBright((prev) => true)
+            }
+            return prev
+          })
+      }, 10);
+      const interval3 = setInterval(() => {
+        setPlugins((prev) => {
+            if(prev < 200){
+              prev += 1
+            }
+             else{
+              prev += 0
+              clearInterval(interval3)
+            }
+            return prev
+          })
+      }, 40);
+    }, 75);
+  }, [])
+
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2>About company</h2>
+        <h1>We create solutions for all types of problems</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Quidem cumque in iure reiciendis dolorum recusandae voluptas
+          necessitatibus facilis sed, harum reprehenderit.</p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>{expNumber}+</h1>
+            <p className={isBright && styles.bright}>Years of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>{reached}+</h1>
+            <p className={isBright && styles.bright}>Companies reached</p>
+          </div>
+          <div className={styles.box}>
+            <h1>{plugins}+</h1>
+            <p className={isBright && styles.bright}>Services and plugins</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
         <Image src={"/coding.png"} alt="man coding" fill/>
       </div>
