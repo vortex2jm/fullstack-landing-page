e0094e381a6c0e5fa10dea341978e026b7f23348
vortex2jm <joaopaulomoura5328@gmail.com>
2024-01-31 01:03:59 -0300

    :sparkles: Adding utilities

---
+++ src/lib/utils.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+import axios from "axios"
+
+const connection = {}
+
+export const db_connect = async () => {
+  try {
+    if(connection.isConnected)
+      return
+    const db = await mongoose.connect(process.env.MONGO)
+    connection.isConnected = db.connections[0].readyState
+  } catch (error) {
+    console.log(error)
+    throw new Error(error)
+  }
+}
+
+export const axiosClient = axios.create({
+  baseURL: process.env.BASE_URL,
+  headers: {
+    "apikey": process.env.API_KEY
+  }
+})
